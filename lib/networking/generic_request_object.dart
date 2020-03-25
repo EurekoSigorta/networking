14885a7aa0e4925360a847664407a3d0ca9a6c00
Serhan Kılıçarslan <serhankilicarslan@gmail.com>
2020-03-25 13:42:41 +0300

    Improvement has been made so that the timeout parameter can be changed per request.

--- lib/networking/generic_request_object.dart
+++ lib/networking/generic_request_object.dart
@@ -45,7 +45,6 @@ class GenericRequestObject<ResponseType extends Serializable> {
   ]) {
     _headers = new Set();
     _cookies = new Set();
-    _timeout = Duration(seconds: 60);
     _isParse = false;
   }
 
@@ -165,8 +164,7 @@ class GenericRequestObject<ResponseType extends Serializable> {
 
   Future<HttpClientRequest> _request() async {
     final client = HttpClient();
-    client.connectionTimeout =
-        _config == null ? Duration(minutes: 1) : _config.timeout;
+    client.connectionTimeout = _getRequestTimeLimit;
     switch (_methodType) {
       case MethodType.GET:
         return await client.getUrl(_uri);
@@ -184,8 +182,9 @@ class GenericRequestObject<ResponseType extends Serializable> {
   }
 
   Future<dynamic> fetch() async {
-    var request = await _request();
+    var request;
     try {
+      request = await _request();
       _cookies?.forEach((cookie) => request.cookies.add(cookie));
       _headers
           .forEach((header) => request.headers.add(header.key, header.value));
@@ -237,9 +236,7 @@ class GenericRequestObject<ResponseType extends Serializable> {
       }
 
       var response = await request.close().timeout(
-        _config != null
-            ? _config.timeout
-            : _timeout == null ? Duration(minutes: 1) : _timeout,
+        _getRequestTimeLimit,
         onTimeout: () {
           throw TimeoutException("Timeout");
         },
@@ -368,6 +365,10 @@ class GenericRequestObject<ResponseType extends Serializable> {
   void enqueue() {
     NetworkQueue.instance.add(this);
   }
+
+  Duration get _getRequestTimeLimit => _timeout != null
+      ? _timeout
+      : _config != null ? _config.timeout : Duration(minutes: 1);
 
   @override
   bool operator ==(Object other) =>
